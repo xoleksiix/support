bf31c5f176055013a505e0c3b1feedb1029dc18b
John Doe <johndoe@example.com>
2022-07-23 18:25:38 +0300

    create

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,4 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path("home/", home),
-    # Exchange rates
-    #path("btc_usd/", btc_usd),
-    #path("history/", history),
-    # Tickets
-    #path("tickets/", get_all_tickets),
 ]
