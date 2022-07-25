e626b8f2c86df3d361cd99b008c0ab218fe2dfec
oleksii <johndoe@example.com>
2022-07-25 18:43:17 +0300

    create

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from config.api import btc_usd, history, home
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path("home/", home),
-    # Exchange rates
-    #path("btc_usd/", btc_usd),
-    #path("history/", history),
-    # Tickets
-    #path("tickets/", get_all_tickets),
+    path("home/", home),
+    path("btc_usd/", btc_usd),
+    path("history/", history),
 ]
