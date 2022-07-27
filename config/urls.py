01e6834006f80cd28eeefcf12beb10783d446715
oleksii <johndoe@example.com>
2022-07-27 19:44:42 +0300

    move api

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from config.api import btc_usd, history, home
+from config.api.exchange_rates import btc_usd, history, home
+from config.api.tickets import get_all_tickets
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", home),
+    # Exchange rates
     path("btc_usd/", btc_usd),
     path("history/", history),
+    # Tickets
+    path("tickets/", get_all_tickets),
 ]
